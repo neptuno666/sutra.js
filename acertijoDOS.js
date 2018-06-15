620282ad4db5bcf3f0ee01993ca447c32bf0ad8c
neptuno666 <40292480+neptuno666@users.noreply.github.com>
2018-06-15 02:56:01 -0700

    Update acertijoDOS.js
    changing variables to please the master

--- acertijoDOS.js
+++ acertijoDOS.js
@@ -1,13 +1,31 @@
 // perra bida
 
-const problem = {
-  start: {A: 1, B: 9, C: 14},
-  A: {B: 10, D: 15},
-  B: {C: 2, D: 11},
-  C: {finish: 9},
-  D: {finish: 6},
-  finish: {}
+const graph = {
+  UNO: {A: 1, B: 9, C: 14},
+  DOS: {B: 10, D:15},
+  TRES: {C: 2, D: 11},
+  SEIS: {finish: 9},
+  CUATRO: {finish: 6},
+  CINCO: {}
 };
+
+const costs = {
+  A: 1,
+  B: 9,
+  C: 14,
+  finish: Infinity 
+};
+
+const parents = {
+  A: 'start',
+  B: 'start',
+  C: 'start',
+  finish: null
+};
+
+const processed = ["start", "A", "B", "C"];
+
+console.log(costs)
 
 const lowestCostNode = (costs, processed)  => {
   return Object.keys(costs).reduce((lowest, node) => {
@@ -20,23 +38,21 @@ const lowestCostNode = (costs, processed)  => {
   }, null);
 };
 
-// function that returns the minimum cost and path to reach Finish
 const sutra = (graph) => {
-  
-  // track paths
   const costs = Object.assign({finish: Infinity}, graph.start);
   const parents = {finish: null};
+
   for (let child in graph.start) { // add children of start node
     parents[child] = 'start';
   }
 
-  // track nodes that have already been processed
   const processed = [];
   
   let node = lowestCostNode(costs, processed);
   while (node) {
     let cost = costs[node];
     let children = graph[node];
+
     for (let n in children) {
       let newCost = cost + children[n];
       if (!costs[n]) {
@@ -48,16 +64,20 @@ const sutra = (graph) => {
         parents[n] = node;
       }
     }
+
     processed.push(node);
+
     node = lowestCostNode(costs, processed);
   }
 
   let optimalPath = ['finish'];
   let parent = parents.finish;
+
   while (parent) {
     optimalPath.push(parent);
     parent = parents[parent];
   }
+
   optimalPath.reverse(); // reverse array to get correct order
 
   const results = {
@@ -67,5 +87,3 @@ const sutra = (graph) => {
 
   return results;
 }; // end of function
-
-console.log(sutra(problem));
